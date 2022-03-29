affee43ee0ff9f2e7e1e853ac2fde8bf591caaa3
99rbbt <lion990717@gmail.com>
2022-03-29 22:43:16 +0900

    hw5

---
+++ Session5_HW/counter/count/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def countviews(request):
+    return render(request, 'counthtml.html')
+
+def resultviews(request):
+    text = request.POST['text']
+    total_len = len(text)
+    no_blank_len = len(text.replace(' ', ''))
+    words = len(text.split(' '))
+    return render(request, 'resulthtml.html', {'text': text, 'total_len': total_len, 'no_blank_len': no_blank_len, 'words': words})
